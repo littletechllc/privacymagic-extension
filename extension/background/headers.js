e058dd99bfb6e01a875a439fa00c63a58b2564c9
Arthur Edelstein <arthuredelstein@gmail.com>
2025-10-17 19:27:29 -0700

    cleanup

--- extension/background/headers.js
+++ extension/background/headers.js
@@ -1,19 +1,4 @@
-export const HEADERS_TO_SET = {
-  "Sec-CH-UA-Arch": "arm",
-  "Sec-CH-UA-Bitness": "64",
-  "Sec-CH-UA-Form-Factors-List": "Desktop",
-  "Sec-CH-UA-Form-Factors": "Desktop",
-  "Sec-CH-UA-Full-Version-List": "Google Chrome;v=\"141.0.0.0\", Not?A_Brand;v=\"8.0.0.0\", Chromium;v=\"141.0.0.0\"",
-  "Sec-CH-UA-Full-Version": "141.0.0.0",
-  "Sec-CH-UA-Mobile": "?0",
-  "Sec-CH-UA-Model": "",
-  "Sec-CH-UA-Platform-Version": "13.0.0",
-  "Sec-CH-UA-Platform": "Windows",
-  "Sec-CH-UA": "Google Chrome;v=\"141\", Not?A_Brand;v=\"8\", Chromium;v=\"141\"",
-  "Sec-GPC": "1",
-}
-
-export const HEADERS_TO_REMOVE = [
+const HEADERS_TO_REMOVE = [
   "Device-Memory",
   "Downlink",
   "DPR",
@@ -30,6 +15,21 @@ export const HEADERS_TO_REMOVE = [
   "Sec-CH-Viewport-Width",
   "Viewport-Width",
 ]
+
+const HEADERS_TO_SET = {
+    "Sec-CH-UA-Arch": "arm",
+    "Sec-CH-UA-Bitness": "64",
+    "Sec-CH-UA-Form-Factors-List": "Desktop",
+    "Sec-CH-UA-Form-Factors": "Desktop",
+    "Sec-CH-UA-Full-Version-List": "Google Chrome;v=\"141.0.0.0\", Not?A_Brand;v=\"8.0.0.0\", Chromium;v=\"141.0.0.0\"",
+    "Sec-CH-UA-Full-Version": "141.0.0.0",
+    "Sec-CH-UA-Mobile": "?0",
+    "Sec-CH-UA-Model": "",
+    "Sec-CH-UA-Platform-Version": "13.0.0",
+    "Sec-CH-UA-Platform": "Windows",
+    "Sec-CH-UA": "Google Chrome;v=\"141\", Not?A_Brand;v=\"8\", Chromium;v=\"141\"",
+    "Sec-GPC": "1",
+}
 
 export const generateRequestHeaders = () => [
   ...HEADERS_TO_REMOVE.map((header) => ({ header, operation: "remove"})),
