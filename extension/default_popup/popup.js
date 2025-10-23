20512cc99746f38a0cfeda7042ebc9de08267b9a
Arthur Edelstein <arthuredelstein@gmail.com>
2025-10-23 11:38:48 -0700

    popup domain and favicon

--- extension/default_popup/popup.js
+++ extension/default_popup/popup.js
@@ -1,5 +1,24 @@
+import psl from '../thirdparty/psl.mjs';
+
 document.getElementById('settingsButton').addEventListener('click', function() {
-    console.log('settingsButton clicked');
-    chrome.runtime.openOptionsPage();
+  console.log('settingsButton clicked');
+  chrome.runtime.openOptionsPage();
 });
 
+const faviconURL = (pageUrl) => {
+  const url = new URL(chrome.runtime.getURL("/_favicon/"));
+  url.searchParams.set("pageUrl", pageUrl);
+  url.searchParams.set("size", "24");
+  return url.toString();
+}
+
+const updateSiteInfo = async () => {
+  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  const tab = tabs[0];
+  const url = tab.url;
+  const domain = psl.get(new URL(url).hostname);
+  document.getElementById('domain').textContent = domain;
+  document.getElementById('favicon').src = faviconURL(url);
+}
+
+await updateSiteInfo();
