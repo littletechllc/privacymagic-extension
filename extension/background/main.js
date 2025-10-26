c6b8468d19a5f2fdc96aeb5cb018e7c5acc66341
Arthur Edelstein <arthur@littletechllc.com>
2025-10-26 07:17:13 -0700

    wire css injection to setting

--- extension/background/main.js
+++ extension/background/main.js
@@ -1,22 +1,26 @@
 import { generateToolbarIcon } from './icon-generator.js'
 import { initializeDynamicRules, clearDynamicRules } from './rules-generator.js'
-import psl from '../thirdparty/psl.mjs';
 
+import psl from '../thirdparty/psl.mjs';
+import { getSetting } from '../common/settings.js';
 
 const injectCssForCosmeticFilters = () => {
   chrome.webNavigation.onCommitted.addListener(async (details) => {
     const url = new URL(details.url);
     const registrableDomain = psl.get(url.hostname);
-    const files = [
-      'content_scripts/adblock_css/_default_.css',
-      `content_scripts/adblock_css/${registrableDomain}_.css`
-    ];
+    const setting = await getSetting(registrableDomain, 'blocking', 'ads', true);
+    if (!setting) {
+      return;
+    }
     chrome.scripting.insertCSS({
       target: {
         tabId: details.tabId,
         frameIds: [details.frameId]
       },
-      files
+      [
+        'content_scripts/adblock_css/_default_.css',
+        `content_scripts/adblock_css/${registrableDomain}_.css`
+      ]
     });
   }, {urls: ["<all_urls>"]});
 }
