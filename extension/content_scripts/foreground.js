2b2438c3b60a155d1468ad43dc34a2d6b8a6bdbb
Arthur Edelstein <arthuredelstein@gmail.com>
2025-10-27 11:29:19 -0700

    consolidate to foreground.js

--- extension/content_scripts/foreground.js
+++ extension/content_scripts/foreground.js
@@ -1,8 +1,7 @@
-(function () {
-
-const PRIVACYMAGIC_IGNORE = "__privacymagic_ignore"
+const redefineAPIs = function (exemptions = []) {
 
 //console.log("Hello from content script", window.location.href, top.location?.href)
+
 
 const redefinePropertyValues = (obj, propertyMap) => {
   let properties = {}
@@ -10,21 +9,23 @@ const redefinePropertyValues = (obj, propertyMap) => {
     properties[prop] = { value, writable: true, enumerable: true }
   }
   Object.defineProperties(obj, properties)
-}
+};
 
-const oldMatchMedia = window.matchMedia
+if (!exemptions.includes('screen')) {
+
+const oldMatchMedia = window.matchMedia;
 
 const mediaDeviceToViewport = (mediaQueryString) =>
   mediaQueryString
     ?.replaceAll('device-width', 'width')
-    ?.replaceAll('device-height', 'height')
+    ?.replaceAll('device-height', 'height');
 
 const allowedScreenSizes = [
   [1366, 768],
   [1920, 1080],
   [2560, 1440],
   [3840, 2160]
-]
+];
 
 const spoofScreenSize = (minWidth, minHeight) => {
   for (const [width, height] of allowedScreenSizes) {
@@ -33,7 +34,7 @@ const spoofScreenSize = (minWidth, minHeight) => {
     }
   }
   return allowedScreenSizes[allowedScreenSizes.length - 1]
-}
+};
 
 const [spoofedScreenWidth, spoofedScreenHeight] = spoofScreenSize(innerWidth, innerHeight);
 
@@ -74,47 +75,54 @@ redefinePropertyValues(window, {
   screenX: 0,
   screenY: 0
 });
-
-// window.fetch
-/*
-const BLOBS_IN_LOCALSTORAGE = "__privacymagic_blobs"
-const getBlobUrisInLocalStorage = () =>
-  JSON.parse(localStorage.getItem(BLOBS_IN_LOCALSTORAGE) || '[]')
-const setBlobUrisInLocalStorage = (blobs) =>
-  localStorage.setItem(BLOBS_IN_LOCALSTORAGE, JSON.stringify(blobs))
-const addBlobUriToLocalStorage = (blobUri) => {
-  const blobUris = getBlobUrisInLocalStorage()
-  blobUris.push(blobUri)
-  setBlobUrisInLocalStorage(blobUris)
 }
-const isBlobUriInLocalStorage = (blobUri) => {
-  const blobUris = getBlobUrisInLocalStorage()
-  return blobUris.includes(blobUri)
-}
-const oldFetch = window.fetch
-let stomped = false
-let stompedValue    
-defineStompableGetter(window, "fetch", (resource, ...args) => {
-//        document.body.style.backgroundColor = 'green'
-        console.log('===========================================', resource)
-        const uri = resource?.toString()?.trim()
-        console.log('=======================================', uri)
-        if (uri && uri?.startsWith("blob:") && !isBlobUriInLocalStorage(uri)) {
-          throw new TypeError("Failed to fetch")
-        }
-        return oldFetch(resource, ...args)
-      })
 
-// URL.createObjectURL
-const oldCreateObjectURL = URL.createObjectURL
-Object.defineProperty(URL, "createObjectURL", {
-  get: () => {
-    return ((obj) => {
-      const blobUri = oldCreateObjectURL(obj)
-      addBlobUriToLocalStorage(blobUri)
-      return blobUri
-    })
+if (!exemptions.includes('window_name')) {
+
+  const propDescriptor = Object.getOwnPropertyDescriptor(window, 'name');
+  if (!propDescriptor) {
+    return;
   }
-})
-*/
-})()
+  const nameGetter = propDescriptor.get;
+  const nameSetter = propDescriptor.set;
+  Object.defineProperty(window, 'name', {
+    get() {
+      const nameStr = nameGetter.call(this);
+      try {
+        const data = JSON.parse(nameStr);
+        if (typeof data !== 'object' || data === null) {
+          return '';
+        }
+        const origin = window.location.origin;
+        if (typeof data[origin] !== 'string') {
+          return '';
+        }
+        return data[origin];
+      } catch (error) {
+        return '';
+      }
+    },
+    set(value) {
+      const nameStr = nameGetter.call(this);
+      let data;
+      try { 
+        data = JSON.parse(nameStr);
+        if (typeof data !== 'object' || data === null) {
+          data = {};
+        }
+      } catch (error) {
+        data = {};
+      }
+      const origin = window.location.origin;
+      if (!origin || origin.length === 0) {
+        return;
+      }
+      // String(value) matches window.name native behavior
+      data[origin] = String(value);
+      nameSetter.call(this, JSON.stringify(data));
+    },
+    configurable: true
+  });
+}
+
+};
