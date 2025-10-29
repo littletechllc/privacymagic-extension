c1cc9453c9a89aacbf8597564eea3e71a38c2aaa
Arthur Edelstein <arthur@littletechllc.com>
2025-10-28 19:30:40 -0700

    subresource header rules

--- extension/background/main.js
+++ extension/background/main.js
@@ -3,7 +3,7 @@ import { initializeDynamicRules, clearDynamicRules } from './rules-generator.js'
 import { injectCssForCosmeticFilters } from './cosmetic-filter-manager.js';
 import { THEME_CONFIG } from '../common/theme.js';
 import { updateContentScripts, createContentScripts } from './content-scripts.js';
-import { listenForSettingsChanges, setSetting, getAllSettings, SETTINGS_KEY_PREFIX } from '../common/settings.js';
+import { listenForSettingsChanges, getSetting, setSetting, getAllSettings, SETTINGS_KEY_PREFIX } from '../common/settings.js';
 import psl from '../thirdparty/psl.mjs';
 
 const PRIVACY_MAGIC_HEADERS = {
@@ -82,6 +82,20 @@ const updateTopLevelHeaderRule = async (domain, settingId, value) => {
   });
 };
 
+const setupTopLevelHeaderRules = async () => {
+  // Create the top level header rule, without any excluded request domains.
+  const allSettings = await getAllSettings();
+  for (const settingId of Object.keys(PRIVACY_MAGIC_HEADERS)) {
+    await createTopLevelHeaderRule(settingId);
+  }
+  // Add necessary excluded request domains for the top level header rule.
+  for (const [[type, domain, settingId], value] of allSettings) {
+    if (type === SETTINGS_KEY_PREFIX && settingId in PRIVACY_MAGIC_HEADERS) {
+      await updateTopLevelHeaderRule(domain, settingId, value);
+    }
+  }
+};
+
 // Create the subresource header rule, without any excluded tab ids.
 const createSubresourceHeaderRule = async (settingId) => {
   const { headers, id } = PRIVACY_MAGIC_HEADERS[settingId];
@@ -89,7 +103,7 @@ const createSubresourceHeaderRule = async (settingId) => {
     ([header, value]) => ({operation: "set", header, value}))
     const rules = [
       {
-        id: 1000 + id,
+        id: 2500 + id,
         priority: 1,
         action: {
           type: "modifyHeaders",
@@ -106,79 +120,56 @@ const createSubresourceHeaderRule = async (settingId) => {
 }
 
 // Add or remove a tab id from the excluded tab ids for the subresource header rule.
-const toggleExcludedTabIdsForSubresourceHeader = async (settingId, tabId, value) => {
-  const { headers, id } = PRIVACY_MAGIC_HEADERS[settingId];
+const updateSubresourceHeaderRule = async (settingId, tabId, value) => {
+  const { id } = PRIVACY_MAGIC_HEADERS[settingId];
   let rules = await chrome.declarativeNetRequest.getSessionRules({
-    ruleIds: [id + 1000],
+    ruleIds: [2500 + id],
   });
-  for (const rule of rules) {
-    if (value === false) {
-      if (!rule.condition.excludedTabIds.includes(tabId)) {
-        rule.condition.excludedTabIds.push(tabId);
-      }
-    } else {
-      if (rule.condition.excludedTabIds.includes(tabId)) {
-        rule.condition.excludedTabIds = rule.condition.excludedTabIds.filter(t => t !== tabId);
-      }
+  const rule = rules[0];
+  if (value === false) {
+    if (!rule.condition.excludedTabIds.includes(tabId)) {
+      rule.condition.excludedTabIds.push(tabId);
+    }
+  } else {
+    if (rule.condition.excludedTabIds.includes(tabId)) {
+      rule.condition.excludedTabIds = rule.condition.excludedTabIds.filter(t => t !== tabId);
     }
   }
   await chrome.declarativeNetRequest.updateSessionRules({
-    removeRuleIds: rules.map(rule => rule.id),
-    addRules: rules,
+    removeRuleIds: [rule.id],
+    addRules: [rule],
   });
 };
 
-const setupTopLevelHeaders = async () => {
-  // Create the top level header rule, without any excluded request domains.
-  const allSettings = await getAllSettings();
-  for (const settingId of Object.keys(PRIVACY_MAGIC_HEADERS)) {
-    await createTopLevelHeaderRule(settingId);
-  }
-  // Add necessary excluded request domains for the top level header rule.
-  for (const [[type, domain, settingId], value] of allSettings) {
-    if (type === SETTINGS_KEY_PREFIX && settingId in PRIVACY_MAGIC_HEADERS) {
-      await updateTopLevelHeaderRule(domain, settingId, value);
-    }
-  }
-};
-
-const setupSubresourceHeaders = async () => {
+const setupSubresourceHeaderRules = async () => {
   // Create the subresource header rules, without any excluded tab ids.
   for (const settingId of Object.keys(PRIVACY_MAGIC_HEADERS)) {
     await createSubresourceHeaderRule(settingId);
   }
-  // Wait for a top-level request and add the appropriate excluded tab ids
-  // for the subresource header rule.
-  chrome.webRequest.onBeforeRequest.addListener(async (details) => {
-    const domain = psl.get(new URL(details.url).hostname);
+  // Wait for a top-level request or navigation and exclude the tabId from the subresource header rule.
+  const listener = async ({url, tabId}) => {
+    const domain = psl.get(new URL(url).hostname);
     if (domain === null) {
       return;
     }
     for (const settingId of Object.keys(PRIVACY_MAGIC_HEADERS)) {
-      const setting = await getSetting(registrableDomain, settingId);
-      const value = await getSetting(SETTINGS_KEY_PREFIX, domain, 'connections', 'headers', settingId);
-      if (value === true) {
-        await toggleExcludedTabIdsForSubresourceHeader(settingId, details.tabId, true);
-      }
-    }
-      await toggleExcludedTabIdsForSubresourceHeader(settingId, details.tabId, true);
-  }, {urls: ["<all_urls>"], types: ["main_frame"]});
-  const allSettings = await getAllSettings();
-  for (const [[type, domain, settingId], value] of allSettings) {
-    if (type === SETTINGS_KEY_PREFIX && settingId in PRIVACY_MAGIC_HEADERS) {
-      await toggleExcludedDomainForSubresourceHeader(settingId, domain, value);
+      const setting = await getSetting(domain, settingId);
+      await updateSubresourceHeaderRule(settingId, tabId, setting);
     }
   }
+  chrome.webRequest.onBeforeRequest.addListener(listener, {urls: ["<all_urls>"], types: ["main_frame"]});
+  chrome.webNavigation.onCommitted.addListener(listener, {urls: ["<all_urls>"]});
 };
-const setupHeaders = async () => {
-  await setupTopLevelHeaders();
-  //await setupSubresourceHeaders();
+
+const setupHeaderRules = async () => {
+  await setupTopLevelHeaderRules();
+  await setupSubresourceHeaderRules();
 };
 
 const updateSetting = async (domain, settingId, value) => {
   await setSetting(domain, settingId, value);
   await updateContentScripts(domain, settingId, value);
-  await updateHeaderRule(domain, settingId, value);
+  await updateTopLevelHeaderRule(domain, settingId, value);
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -214,7 +205,7 @@ chrome.runtime.onInstalled.addListener(async function (details) {
   }
   const t6 = performance.now();
   console.log(`setupContentScripts took ${t6 - t5} milliseconds`);
-  await setupHeaders();
+  await setupHeaderRules();
 });
 
 chrome.runtime.onStartup.addListener( () => {
