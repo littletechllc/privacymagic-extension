1f262da2faa685316c4ca96ac20bf78b02fd3def
Arthur Edelstein <arthur@littletechllc.com>
2025-11-01 11:34:39 -0700

    correct high-entropy useragent values

--- extension/content_scripts/foreground.js
+++ extension/content_scripts/foreground.js
@@ -110,32 +110,47 @@
       });
     },
     useragent: () => {
-      const ChromeVersion = '141.0.0.0';
-      const ShortChromeVersion = ChromeVersion.split('.')[0];
+      const CHROME_VERSION = '141.0.0.0';
+      const SHORT_CHROME_VERSION = CHROME_VERSION.split('.')[0];
+      const PLATFORM_VERSION = '26.0.0';
+      const BRAND_VERSION = '8.0.0.0';
+      const SHORT_BRAND_VERSION = BRAND_VERSION.split('.')[0];
       redefinePropertyValues(Navigator.prototype, {
-        userAgent: `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${ChromeVersion} Safari/537.36`,
-        appVersion: `5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${ChromeVersion} Safari/537.36`
+        userAgent: `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${CHROME_VERSION} Safari/537.36`,
+        appVersion: `5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${CHROME_VERSION} Safari/537.36`
       });
       redefinePropertyValues(NavigatorUAData.prototype, {
         brands: [
-          { brand: 'Google Chrome', version: ShortChromeVersion },
-          { brand: 'Not?A_Brand', version: '8' },
-          { brand: 'Chromium', version: ShortChromeVersion }
+          { brand: 'Google Chrome', version: SHORT_CHROME_VERSION },
+          { brand: 'Not?A_Brand', version: SHORT_BRAND_VERSION },
+          { brand: 'Chromium', version: SHORT_CHROME_VERSION }
         ],
-        uaFullVersion: ChromeVersion,
-        fullVersionList: [
-          { brand: 'Google Chrome', version: ChromeVersion },
-          { brand: 'Not?A_Brand', version: '8.0.0.0' },
-          { brand: 'Chromium', version: ChromeVersion }
-        ],
-        wow64: false,
-        bitness: 64,
-        platform: 'Windows',
-        architecture: 'arm',
         mobile: false,
-        formFactors: ['Desktop'],
-        platformVersion: '26.0.0',
-        model: ''
+        platform: 'Windows',
+        getHighEntropyValues: async () => (
+          {
+            architecture: 'arm',
+            bitness: '64',
+            brands: [
+              { brand: 'Google Chrome', version: SHORT_CHROME_VERSION },
+              { brand: 'Not?A_Brand', version: SHORT_BRAND_VERSION },
+              { brand: 'Chromium', version: SHORT_CHROME_VERSION }
+            ],
+            formFactors: [
+              'Desktop'
+            ],
+            fullVersionList: [
+              { brand: 'Google Chrome', version: CHROME_VERSION },
+              { brand: 'Not?A_Brand', version: BRAND_VERSION },
+              { brand: 'Chromium', version: CHROME_VERSION }
+            ],
+            mobile: false,
+            model: '',
+            platform: 'macOS',
+            platformVersion: PLATFORM_VERSION,
+            uaFullVersion: CHROME_VERSION,
+            wow64: false
+          })
       });
     },
     battery: () => {
