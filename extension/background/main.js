f0966da3fd7292d77f8423c3b34f2b7cb2018a25
Arthur Edelstein <arthur@littletechllc.com>
2025-11-03 05:41:42 -0800

    cleanup

--- extension/background/main.js
+++ extension/background/main.js
@@ -1,7 +1,6 @@
 /* global chrome */
 
 import { setToolbarIcon } from '../common/toolbar-icon.js';
-import { clearDynamicRules } from './rules-generator.js';
 import { injectCssForCosmeticFilters } from './cosmetic-filter-manager.js';
 import { THEME_CONFIG } from '../common/theme.js';
 import { updateContentScripts, setupContentScripts } from './content-scripts.js';
@@ -36,21 +35,8 @@ const initializeExtension = async () => {
     return;
   }
   await setToolbarIcon(THEME_CONFIG.toolbarIcon);
-  await clearDynamicRules();
-  // await chrome.runtime.openOptionsPage()
-  const t1 = performance.now();
-  // await initializeDynamicRules();
-  const t2 = performance.now();
-  console.log(`initializeDynamicRules took ${t2 - t1} milliseconds`);
-  // const contentBlockingDefinitionsUrl = chrome.runtime.getURL('rules/content-blocking-definitions.json');
-  const t4 = performance.now();
-  // console.log(`fetchJson took ${t4 - t3} milliseconds`);
   injectCssForCosmeticFilters();
-  const t5 = performance.now();
-  console.log(`injectCssForCosmeticFilters took ${t5 - t4} milliseconds`);
   await setupContentScripts();
-  const t6 = performance.now();
-  console.log(`setupContentScripts took ${t6 - t5} milliseconds`);
   await setupHeaderRules();
   console.log('Extension initialized');
   initialized = true;
