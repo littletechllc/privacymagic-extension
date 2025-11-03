4b53089c0566b684ff5b61557f95e5010ab29cd6
Arthur Edelstein <arthuredelstein@gmail.com>
2025-11-03 12:59:27 -0800

    cleanup

--- tools/create-patch-controls.js
+++ tools/create-patch-controls.js
@@ -3,7 +3,8 @@ import * as acorn from 'acorn';
 import esquery from 'esquery';
 import { isMain } from './util.js';
 
-const foregroundScriptPath = '../extension/content_scripts/foreground.js';
+const CONTENT_SCRIPTS_DIR = '../extension/content_scripts';
+const FOREGROUND_SCRIPT_PATH = `${CONTENT_SCRIPTS_DIR}/foreground.js`;
 
 const readFile = async (path) => {
   return await fs.readFile(path, 'utf8');
@@ -33,15 +34,17 @@ const composePatchControlFile = (patchName, enable) => `
 
 const writePatchControlFile = async (patchName, enable) => {
   const content = composePatchControlFile(patchName, enable);
-  const path = `../extension/content_scripts/${enable ? 'enable' : 'disable'}/${patchName}.js`;
+  const path = `${CONTENT_SCRIPTS_DIR}/${enable ? 'enable' : 'disable'}/${patchName}.js`;
   await fs.writeFile(path, content.trim() + '\n');
   console.log(`wrote ${path}`);
 };
 
 const main = async () => {
-  const content = await readFile(foregroundScriptPath);
+  const content = await readFile(FOREGROUND_SCRIPT_PATH);
   const ast = await parseFile(content);
   const patches = getObjectKeys(ast, 'privacyMagicPatches');
+  await fs.mkdir(`${CONTENT_SCRIPTS_DIR}/enable`, { recursive: true });
+  await fs.mkdir(`${CONTENT_SCRIPTS_DIR}/disable`, { recursive: true });
   for (const patch of patches) {
     await writePatchControlFile(patch, true);
     await writePatchControlFile(patch, false);
