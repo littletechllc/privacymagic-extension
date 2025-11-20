271ffe3849f96eb5c60e161d59e8a89a5d08194f
Arthur Edelstein <arthur@littletechllc.com>
2025-11-20 22:24:26 +0200

    gpc comment

--- inject/patches/gpc.js
+++ inject/patches/gpc.js
@@ -2,6 +2,9 @@
 
 import { redefinePropertyValues } from '../helpers.js';
 
+// Global Privacy Control is a signal that allows users to opt out of websites
+// selling or sharing their personal information with third parties.
+// https://globalprivacycontrol.org/
 const gpc = () => {
   console.log('gpc patch', self.location.href);
   const navigatorPrototype = self.Navigator || self.WorkerNavigator;
