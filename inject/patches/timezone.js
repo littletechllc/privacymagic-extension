69e6242f758c14b944e3a78f66c20f63516f863c
Arthur Edelstein <arthur@littletechllc.com>
2025-11-20 22:16:43 +0200

    western hemisphere dst timezones

--- inject/patches/timezone.js
+++ inject/patches/timezone.js
@@ -4,6 +4,9 @@
 import { reflectApplySafe, redefinePropertyValues } from '../helpers.js';
 
 const timezone = () => {
+  // Time zones that use fractional hour offsets can be coalesced to a
+  // representative time zone. Must change its offset
+  // on the same dates to coalesce properly.
   const fractionHourTimeZoneMappings = {
     'America/St_Johns': 'America/St_Johns',
     'Asia/Calcutta': 'Asia/Kolkata',
@@ -28,7 +31,126 @@ const timezone = () => {
     'NZ-CHAT': 'Pacific/Chatham',
     'Pacific/Chatham': 'Pacific/Chatham'
   };
+
+  // Time zones that use DST can be coalesced to a
+  // representative time zone. Must change its offset
+  // on the same dates to coalesce properly.
   const dstTimeZoneMappings = {
+
+    // HST -> HDT
+    'America/Adak': 'America/Adak',
+    'America/Atka': 'America/Adak',
+    'US/Aleutian': 'America/Adak',
+
+    // AKST -> AKDT
+    'America/Anchorage': 'America/Anchorage',
+    'America/Juneau': 'America/Anchorage',
+    'America/Metlakatla': 'America/Anchorage',
+    'America/Nome': 'America/Anchorage',
+    'America/Sitka': 'America/Anchorage',
+    'America/Yakutat': 'America/Anchorage',
+    'US/Alaska': 'America/Anchorage',
+
+    // PST -> PDT
+    'America/Ensenada': 'America/Los_Angeles',
+    'America/Los_Angeles': 'America/Los_Angeles',
+    'America/Santa_Isabel': 'America/Los_Angeles',
+    'America/Tijuana': 'America/Los_Angeles',
+    'America/Vancouver': 'America/Los_Angeles',
+    'Canada/Pacific': 'America/Los_Angeles',
+    'Mexico/BajaNorte': 'America/Los_Angeles',
+    'PST8PDT': 'America/Los_Angeles',
+    'US/Pacific': 'America/Los_Angeles',
+
+    // MST -> MDT
+    'America/Boise': 'America/Denver',
+    'America/Cambridge_Bay': 'America/Denver',
+    'America/Ciudad_Juarez': 'America/Denver',
+    'America/Denver': 'America/Denver',
+    'America/Edmonton': 'America/Denver',
+    'America/Inuvik': 'America/Denver',
+    'America/Shiprock': 'America/Denver',
+    'America/Yellowknife': 'America/Denver',
+    'Canada/Mountain': 'America/Denver',
+    'MST7MDT': 'America/Denver',
+    'Navajo': 'America/Denver',
+    'US/Mountain': 'America/Denver',
+
+    // CST -> CDT
+    'America/Indiana/Knox': 'America/Chicago',
+    'America/Indiana/Tell_City': 'America/Chicago',
+    'America/Knox_IN': 'America/Chicago',
+    'America/Chicago': 'America/Chicago',
+    'America/Matamoros': 'America/Chicago',
+    'America/Menominee': 'America/Chicago',
+    'America/North_Dakota/Beulah': 'America/Chicago',
+    'America/North_Dakota/Center': 'America/Chicago',
+    'America/North_Dakota/New_Salem': 'America/Chicago',
+    'America/Ojinaga': 'America/Chicago',
+    'America/Rainy_River': 'America/Chicago',
+    'America/Rankin_Inlet': 'America/Chicago',
+    'America/Resolute': 'America/Chicago',
+    'America/Winnipeg': 'America/Chicago',
+    'Canada/Central': 'America/Chicago',
+    'CST6CDT': 'America/Chicago',
+    'US/Central': 'America/Chicago',
+    'US/Indiana-Starke': 'America/Chicago',
+
+    // -6 -> -5
+    'Pacific/Easter': 'Pacific/Easter',
+    'Chile/EasterIsland': 'Pacific/Easter',
+
+    // EST -> EDT
+    'America/Detroit': 'America/New_York',
+    'America/Fort_Wayne': 'America/New_York',
+    'America/Grand_Turk': 'America/New_York',
+    'America/Indiana/Indianapolis': 'America/New_York',
+    'America/Indiana/Marengo': 'America/New_York',
+    'America/Indiana/Petersburg': 'America/New_York',
+    'America/Indiana/Vevay': 'America/New_York',
+    'America/Indiana/Vincennes': 'America/New_York',
+    'America/Indiana/Winamac': 'America/New_York',
+    'America/Iqaluit': 'America/New_York',
+    'America/Kentucky/Louisville': 'America/New_York',
+    'America/Kentucky/Monticello': 'America/New_York',
+    'America/Louisville': 'America/New_York',
+    'America/Montreal': 'America/New_York',
+    'America/Nassau': 'America/New_York',
+    'America/New_York': 'America/New_York',
+    'America/Nipigon': 'America/New_York',
+    'America/Pangnirtung': 'America/New_York',
+    'America/Paramaribo': 'America/New_York',
+    'America/Port-au-Prince': 'America/New_York',
+    'America/Thunder_Bay': 'America/New_York',
+    'America/Toronto': 'America/New_York',
+    'Canada/Eastern': 'America/New_York',
+    'EST5EDT': 'America/New_York',
+    'US/East-Indiana': 'America/New_York',
+    'US/Eastern': 'America/New_York',
+    'US/Michigan': 'America/New_York',
+    'US/Northwest-Indiana': 'America/New_York',
+
+    // AST -> ADT
+    'America/Glace_Bay': 'America/Halifax',
+    'America/Goose_Bay': 'America/Halifax',
+    'America/Halifax': 'America/Halifax',
+    'America/Moncton': 'America/Halifax',
+    'America/Thule': 'America/Halifax',
+    'Atlantic/Bermuda': 'America/Halifax',
+    'Canada/Atlantic': 'America/Halifax',
+
+    // -04 -> -03
+    'America/Santiago': 'America/Santiago',
+    'Chile/Continental': 'America/Santiago',
+
+    // -03 -> -02
+    'America/Miquelon': 'America/Miquelon',
+
+    // -02 -> -01
+    'America/Godthab': 'America/Nuuk',
+    'America/Nuuk': 'America/Nuuk',
+    'America/Scoresbysund': 'America/Nuuk',
+
     // GMT -> BST
     'Europe/Belfast': 'Europe/London',
     'Europe/Guernsey': 'Europe/London',
@@ -123,14 +245,17 @@ const timezone = () => {
   };
   const originalResolvedOptions = Intl.DateTimeFormat.prototype.resolvedOptions;
   const originalResolvedOptionsSafe = (intlDateTimeFormat) => reflectApplySafe(originalResolvedOptions, intlDateTimeFormat, []);
+  const originalMathTrunc = Math.trunc;
+  const originalDateGetTimezoneOffset = Date.prototype.getTimezoneOffset;
+  const originalDateGetTimezoneOffsetSafe = (date) => reflectApplySafe(originalDateGetTimezoneOffset, date, []);
+  const OriginalDate = Date;
   return redefinePropertyValues(Intl.DateTimeFormat.prototype, {
     resolvedOptions: function () {
       const options = originalResolvedOptionsSafe(this);
-      console.log('original time zone: ', options.timeZone);
-      const now = new Date();
+      const now = new OriginalDate();
       // negative to match ISO 8061 sign convention:
-      const offsetMinutes = -now.getTimezoneOffset();
-      const hours = Math.trunc(offsetMinutes / 60);
+      const offsetMinutes = -originalDateGetTimezoneOffsetSafe(now);
+      const hours = originalMathTrunc(offsetMinutes / 60);
       const minutes = offsetMinutes % 60;
       // If the time zone has a minute offset, return the coalesced time zone.
       if (minutes !== 0) {
