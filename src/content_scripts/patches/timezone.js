5cc212c711a128ada41e138b2aba9ba03b1ab74e
Arthur Edelstein <arthur@littletechllc.com>
2025-12-05 07:11:06 -0800

    use constants for representative timezones

--- src/content_scripts/patches/timezone.js
+++ src/content_scripts/patches/timezone.js
@@ -7,283 +7,317 @@ const timezone = () => {
   // Time zones that use fractional hour offsets can be coalesced to a
   // representative time zone. Must change its offset
   // on the same dates to coalesce properly.
+
+  const AsiaKathmandu = 'Asia/Kathmandu';
+  const AsiaKolkata = 'Asia/Kolkata';
+  const AsiaTehran = 'Asia/Tehran';
+  const AsiaYangon = 'Asia/Yangon';
+  const AustraliaDarwin = 'Australia/Darwin';
+  const PacificChatham = 'Pacific/Chatham';
+  const PacificMarquesas = 'Pacific/Marquesas';
+
   const fractionHourTimeZoneMappings = {
-    'Asia/Calcutta': 'Asia/Kolkata',
-    'Asia/Colombo': 'Asia/Kolkata',
-    'Asia/Kathmandu': 'Asia/Kathmandu',
-    'Asia/Katmandu': 'Asia/Kathmandu',
-    'Asia/Kolkata': 'Asia/Kolkata',
-    'Asia/Rangoon': 'Asia/Yangon',
-    'Asia/Tehran': 'Asia/Tehran',
-    'Asia/Yangon': 'Asia/Yangon',
-    'Australia/Darwin': 'Australia/Darwin',
-    'Australia/North': 'Australia/Darwin',
-    'Indian/Cocos': 'Asia/Yangon',
-    'Iran': 'Asia/Tehran',
-    'NZ-CHAT': 'Pacific/Chatham',
-    'Pacific/Chatham': 'Pacific/Chatham',
-    'Pacific/Marquesas': 'Pacific/Marquesas'
+    'Asia/Calcutta': AsiaKolkata,
+    'Asia/Colombo': AsiaKolkata,
+    'Asia/Kathmandu': AsiaKathmandu,
+    'Asia/Katmandu': AsiaKathmandu,
+    'Asia/Kolkata': AsiaKolkata,
+    'Asia/Rangoon': AsiaYangon,
+    'Asia/Tehran': AsiaTehran,
+    'Asia/Yangon': AsiaYangon,
+    'Australia/Darwin': AustraliaDarwin,
+    'Australia/North': AustraliaDarwin,
+    'Indian/Cocos': AsiaYangon,
+    'Iran': AsiaTehran,
+    'NZ-CHAT': PacificChatham,
+    'Pacific/Chatham': PacificChatham,
+    'Pacific/Marquesas': PacificMarquesas
   };
 
   // Time zones that use DST can be coalesced to a
   // representative time zone. Must change its offset
   // on the same dates to coalesce properly.
+
+  const AfricaCairo = 'Africa/Cairo';
+  const AfricaCasablanca = 'Africa/Casablanca';
+  const AmericaAdak = 'America/Adak';
+  const AmericaAnchorage = 'America/Anchorage';
+  const AmericaChicago = 'America/Chicago';
+  const AmericaDenver = 'America/Denver';
+  const AmericaHalifax = 'America/Halifax';
+  const AmericaLosAngeles = 'America/Los_Angeles';
+  const AmericaMiquelon = 'America/Miquelon';
+  const AmericaNewYork = 'America/New_York';
+  const AmericaNuuk = 'America/Nuuk';
+  const AmericaSantiago = 'America/Santiago';
+  const AmericaStJohns = 'America/St_Johns';
+  const AntarcticaTroll = 'Antarctica/Troll';
+  const AsiaGaza = 'Asia/Gaza';
+  const AsiaJerusalem = 'Asia/Jerusalem';
+  const AtlanticAzores = 'Atlantic/Azores';
+  const AustraliaAdelaide = 'Australia/Adelaide';
+  const AustraliaLordHowe = 'Australia/Lord_Howe';
+  const AustraliaSydney = 'Australia/Sydney';
+  const EuropeAthens = 'Europe/Athens';
+  const EuropeBerlin = 'Europe/Berlin';
+  const EuropeLondon = 'Europe/London';
+  const PacificAuckland = 'Pacific/Auckland';
+  const PacificEaster = 'Pacific/Easter';
+  const PacificNorfolk = 'Pacific/Norfolk';
+
   const dstTimeZoneMappings = {
 
     // HST -> HDT
-    'America/Adak': 'America/Adak',
-    'America/Atka': 'America/Adak',
-    'US/Aleutian': 'America/Adak',
+    'America/Adak': AmericaAdak,
+    'America/Atka': AmericaAdak,
+    'US/Aleutian': AmericaAdak,
 
     // AKST -> AKDT
-    'America/Anchorage': 'America/Anchorage',
-    'America/Juneau': 'America/Anchorage',
-    'America/Metlakatla': 'America/Anchorage',
-    'America/Nome': 'America/Anchorage',
-    'America/Sitka': 'America/Anchorage',
-    'America/Yakutat': 'America/Anchorage',
-    'US/Alaska': 'America/Anchorage',
+    'America/Anchorage': AmericaAnchorage,
+    'America/Juneau': AmericaAnchorage,
+    'America/Metlakatla': AmericaAnchorage,
+    'America/Nome': AmericaAnchorage,
+    'America/Sitka': AmericaAnchorage,
+    'America/Yakutat': AmericaAnchorage,
+    'US/Alaska': AmericaAnchorage,
 
     // PST -> PDT
-    'America/Ensenada': 'America/Los_Angeles',
-    'America/Los_Angeles': 'America/Los_Angeles',
-    'America/Santa_Isabel': 'America/Los_Angeles',
-    'America/Tijuana': 'America/Los_Angeles',
-    'America/Vancouver': 'America/Los_Angeles',
-    'Canada/Pacific': 'America/Los_Angeles',
-    'Mexico/BajaNorte': 'America/Los_Angeles',
-    'PST8PDT': 'America/Los_Angeles',
-    'US/Pacific': 'America/Los_Angeles',
+    'America/Ensenada': AmericaLosAngeles,
+    'America/Los_Angeles': AmericaLosAngeles,
+    'America/Santa_Isabel': AmericaLosAngeles,
+    'America/Tijuana': AmericaLosAngeles,
+    'America/Vancouver': AmericaLosAngeles,
+    'Canada/Pacific': AmericaLosAngeles,
+    'Mexico/BajaNorte': AmericaLosAngeles,
+    'PST8PDT': AmericaLosAngeles,
+    'US/Pacific': AmericaLosAngeles,
 
     // MST -> MDT
-    'America/Boise': 'America/Denver',
-    'America/Cambridge_Bay': 'America/Denver',
-    'America/Ciudad_Juarez': 'America/Denver',
-    'America/Denver': 'America/Denver',
-    'America/Edmonton': 'America/Denver',
-    'America/Inuvik': 'America/Denver',
-    'America/Shiprock': 'America/Denver',
-    'America/Yellowknife': 'America/Denver',
-    'Canada/Mountain': 'America/Denver',
-    'MST7MDT': 'America/Denver',
-    'Navajo': 'America/Denver',
-    'US/Mountain': 'America/Denver',
+    'America/Boise': AmericaDenver,
+    'America/Cambridge_Bay': AmericaDenver,
+    'America/Ciudad_Juarez': AmericaDenver,
+    'America/Denver': AmericaDenver,
+    'America/Edmonton': AmericaDenver,
+    'America/Inuvik': AmericaDenver,
+    'America/Shiprock': AmericaDenver,
+    'America/Yellowknife': AmericaDenver,
+    'Canada/Mountain': AmericaDenver,
+    'MST7MDT': AmericaDenver,
+    'Navajo': AmericaDenver,
+    'US/Mountain': AmericaDenver,
 
     // -6 -> -5
-    'Chile/EasterIsland': 'Pacific/Easter',
-    'Pacific/Easter': 'Pacific/Easter',
+    'Chile/EasterIsland': PacificEaster,
+    'Pacific/Easter': PacificEaster,
 
     // CST -> CDT
-    'America/Chicago': 'America/Chicago',
-    'America/Indiana/Knox': 'America/Chicago',
-    'America/Indiana/Tell_City': 'America/Chicago',
-    'America/Knox_IN': 'America/Chicago',
-    'America/Matamoros': 'America/Chicago',
-    'America/Menominee': 'America/Chicago',
-    'America/North_Dakota/Beulah': 'America/Chicago',
-    'America/North_Dakota/Center': 'America/Chicago',
-    'America/North_Dakota/New_Salem': 'America/Chicago',
-    'America/Ojinaga': 'America/Chicago',
-    'America/Rainy_River': 'America/Chicago',
-    'America/Rankin_Inlet': 'America/Chicago',
-    'America/Resolute': 'America/Chicago',
-    'America/Winnipeg': 'America/Chicago',
-    'Canada/Central': 'America/Chicago',
-    'CST6CDT': 'America/Chicago',
-    'US/Central': 'America/Chicago',
-    'US/Indiana-Starke': 'America/Chicago',
+    'America/Chicago': AmericaChicago,
+    'America/Indiana/Knox': AmericaChicago,
+    'America/Indiana/Tell_City': AmericaChicago,
+    'America/Knox_IN': AmericaChicago,
+    'America/Matamoros': AmericaChicago,
+    'America/Menominee': AmericaChicago,
+    'America/North_Dakota/Beulah': AmericaChicago,
+    'America/North_Dakota/Center': AmericaChicago,
+    'America/North_Dakota/New_Salem': AmericaChicago,
+    'America/Ojinaga': AmericaChicago,
+    'America/Rainy_River': AmericaChicago,
+    'America/Rankin_Inlet': AmericaChicago,
+    'America/Resolute': AmericaChicago,
+    'America/Winnipeg': AmericaChicago,
+    'Canada/Central': AmericaChicago,
+    'CST6CDT': AmericaChicago,
+    'US/Central': AmericaChicago,
+    'US/Indiana-Starke': AmericaChicago,
 
     // EST -> EDT
-    'America/Detroit': 'America/New_York',
-    'America/Fort_Wayne': 'America/New_York',
-    'America/Grand_Turk': 'America/New_York',
-    'America/Indiana/Indianapolis': 'America/New_York',
-    'America/Indiana/Marengo': 'America/New_York',
-    'America/Indiana/Petersburg': 'America/New_York',
-    'America/Indiana/Vevay': 'America/New_York',
-    'America/Indiana/Vincennes': 'America/New_York',
-    'America/Indiana/Winamac': 'America/New_York',
-    'America/Iqaluit': 'America/New_York',
-    'America/Kentucky/Louisville': 'America/New_York',
-    'America/Kentucky/Monticello': 'America/New_York',
-    'America/Louisville': 'America/New_York',
-    'America/Montreal': 'America/New_York',
-    'America/Nassau': 'America/New_York',
-    'America/New_York': 'America/New_York',
-    'America/Nipigon': 'America/New_York',
-    'America/Pangnirtung': 'America/New_York',
-    'America/Port-au-Prince': 'America/New_York',
-    'America/Thunder_Bay': 'America/New_York',
-    'America/Toronto': 'America/New_York',
-    'Canada/Eastern': 'America/New_York',
-    'EST5EDT': 'America/New_York',
-    'US/East-Indiana': 'America/New_York',
-    'US/Eastern': 'America/New_York',
-    'US/Michigan': 'America/New_York',
-    'US/Northwest-Indiana': 'America/New_York',
+    'America/Detroit': AmericaNewYork,
+    'America/Fort_Wayne': AmericaNewYork,
+    'America/Grand_Turk': AmericaNewYork,
+    'America/Indiana/Indianapolis': AmericaNewYork,
+    'America/Indiana/Marengo': AmericaNewYork,
+    'America/Indiana/Petersburg': AmericaNewYork,
+    'America/Indiana/Vevay': AmericaNewYork,
+    'America/Indiana/Vincennes': AmericaNewYork,
+    'America/Indiana/Winamac': AmericaNewYork,
+    'America/Iqaluit': AmericaNewYork,
+    'America/Kentucky/Louisville': AmericaNewYork,
+    'America/Kentucky/Monticello': AmericaNewYork,
+    'America/Louisville': AmericaNewYork,
+    'America/Montreal': AmericaNewYork,
+    'America/Nassau': AmericaNewYork,
+    'America/New_York': AmericaNewYork,
+    'America/Nipigon': AmericaNewYork,
+    'America/Pangnirtung': AmericaNewYork,
+    'America/Port-au-Prince': AmericaNewYork,
+    'America/Thunder_Bay': AmericaNewYork,
+    'America/Toronto': AmericaNewYork,
+    'Canada/Eastern': AmericaNewYork,
+    'EST5EDT': AmericaNewYork,
+    'US/East-Indiana': AmericaNewYork,
+    'US/Eastern': AmericaNewYork,
+    'US/Michigan': AmericaNewYork,
+    'US/Northwest-Indiana': AmericaNewYork,
 
     // -04 -> -03
-    'America/Santiago': 'America/Santiago',
-    'Chile/Continental': 'America/Santiago',
+    'America/Santiago': AmericaSantiago,
+    'Chile/Continental': AmericaSantiago,
 
     // AST -> ADT
-    'America/Glace_Bay': 'America/Halifax',
-    'America/Goose_Bay': 'America/Halifax',
-    'America/Halifax': 'America/Halifax',
-    'America/Moncton': 'America/Halifax',
-    'America/Thule': 'America/Halifax',
-    'Atlantic/Bermuda': 'America/Halifax',
-    'Canada/Atlantic': 'America/Halifax',
+    'America/Glace_Bay': AmericaHalifax,
+    'America/Goose_Bay': AmericaHalifax,
+    'America/Halifax': AmericaHalifax,
+    'America/Moncton': AmericaHalifax,
+    'America/Thule': AmericaHalifax,
+    'Atlantic/Bermuda': AmericaHalifax,
+    'Canada/Atlantic': AmericaHalifax,
 
     // NST -> NDT
-    'America/St_Johns': 'America/St_Johns',
-    'Canada/Newfoundland': 'America/St_Johns',
+    'America/St_Johns': AmericaStJohns,
+    'Canada/Newfoundland': AmericaStJohns,
 
     // -03 -> -02
-    'America/Miquelon': 'America/Miquelon',
+    'America/Miquelon': AmericaMiquelon,
 
     // -02 -> -01
-    'America/Godthab': 'America/Nuuk',
-    'America/Nuuk': 'America/Nuuk',
-    'America/Scoresbysund': 'America/Nuuk',
+    'America/Godthab': AmericaNuuk,
+    'America/Nuuk': AmericaNuuk,
+    'America/Scoresbysund': AmericaNuuk,
 
     // -01 -> +00
-    'Atlantic/Azores': 'Atlantic/Azores',
+    'Atlantic/Azores': AtlanticAzores,
 
     // +00 -> +02
-    'Antarctica/Troll': 'Antarctica/Troll',
+    'Antarctica/Troll': AntarcticaTroll,
 
     // WET -> WEST (equivalent to GMT -> BST)
-    'Atlantic/Canary': 'Europe/London',
-    'Atlantic/Faeroe': 'Europe/London',
-    'Atlantic/Faroe': 'Europe/London',
-    'Atlantic/Madeira': 'Europe/London',
-    'Europe/Lisbon': 'Europe/London',
-    'Portugal': 'Europe/London',
-    'WET': 'Europe/London',
+    'Atlantic/Canary': EuropeLondon,
+    'Atlantic/Faeroe': EuropeLondon,
+    'Atlantic/Faroe': EuropeLondon,
+    'Atlantic/Madeira': EuropeLondon,
+    'Europe/Lisbon': EuropeLondon,
+    'Portugal': EuropeLondon,
+    'WET': EuropeLondon,
 
     // GMT -> BST
-    'Europe/Belfast': 'Europe/London',
-    'Europe/Guernsey': 'Europe/London',
-    'Europe/Isle_of_Man': 'Europe/London',
-    'Europe/Jersey': 'Europe/London',
-    'Europe/London': 'Europe/London',
-    'GB-Eire': 'Europe/London',
-    'GB': 'Europe/London',
+    'Europe/Belfast': EuropeLondon,
+    'Europe/Guernsey': EuropeLondon,
+    'Europe/Isle_of_Man': EuropeLondon,
+    'Europe/Jersey': EuropeLondon,
+    'Europe/London': EuropeLondon,
+    'GB-Eire': EuropeLondon,
+    'GB': EuropeLondon,
 
     // GMT -> IST (equivalent to GMT -> BST)
-    'Eire': 'Europe/London',
-    'Europe/Dublin': 'Europe/London',
+    'Eire': EuropeLondon,
+    'Europe/Dublin': EuropeLondon,
 
     // +1 -> +00
-    'Africa/Casablanca': 'Africa/Casablanca',
-    'Africa/El_Aaiun': 'Africa/Casablanca',
+    'Africa/Casablanca': AfricaCasablanca,
+    'Africa/El_Aaiun': AfricaCasablanca,
 
     // CET -> CEST
-    'Africa/Ceuta': 'Europe/Berlin',
-    'Arctic/Longyearbyen': 'Europe/Berlin',
-    'Atlantic/Jan_Mayen': 'Europe/Berlin',
-    'CET': 'Europe/Berlin',
-    'Europe/Amsterdam': 'Europe/Berlin',
-    'Europe/Andorra': 'Europe/Berlin',
-    'Europe/Belgrade': 'Europe/Berlin',
-    'Europe/Berlin': 'Europe/Berlin',
-    'Europe/Bratislava': 'Europe/Berlin',
-    'Europe/Brussels': 'Europe/Berlin',
-    'Europe/Budapest': 'Europe/Berlin',
-    'Europe/Busingen': 'Europe/Berlin',
-    'Europe/Copenhagen': 'Europe/Berlin',
-    'Europe/Gibraltar': 'Europe/Berlin',
-    'Europe/Ljubljana': 'Europe/Berlin',
-    'Europe/Luxembourg': 'Europe/Berlin',
-    'Europe/Madrid': 'Europe/Berlin',
-    'Europe/Malta': 'Europe/Berlin',
-    'Europe/Monaco': 'Europe/Berlin',
-    'Europe/Oslo': 'Europe/Berlin',
-    'Europe/Paris': 'Europe/Berlin',
-    'Europe/Podgorica': 'Europe/Berlin',
-    'Europe/Prague': 'Europe/Berlin',
-    'Europe/Rome': 'Europe/Berlin',
-    'Europe/San_Marino': 'Europe/Berlin',
-    'Europe/Sarajevo': 'Europe/Berlin',
-    'Europe/Skopje': 'Europe/Berlin',
-    'Europe/Stockholm': 'Europe/Berlin',
-    'Europe/Tirane': 'Europe/Berlin',
-    'Europe/Vaduz': 'Europe/Berlin',
-    'Europe/Vatican': 'Europe/Berlin',
-    'Europe/Vienna': 'Europe/Berlin',
-    'Europe/Warsaw': 'Europe/Berlin',
-    'Europe/Zagreb': 'Europe/Berlin',
-    'Europe/Zurich': 'Europe/Berlin',
-    'MET': 'Europe/Berlin',
-    'Poland': 'Europe/Berlin',
+    'Africa/Ceuta': EuropeBerlin,
+    'Arctic/Longyearbyen': EuropeBerlin,
+    'Atlantic/Jan_Mayen': EuropeBerlin,
+    'CET': EuropeBerlin,
+    'Europe/Amsterdam': EuropeBerlin,
+    'Europe/Andorra': EuropeBerlin,
+    'Europe/Belgrade': EuropeBerlin,
+    'Europe/Berlin': EuropeBerlin,
+    'Europe/Bratislava': EuropeBerlin,
+    'Europe/Brussels': EuropeBerlin,
+    'Europe/Budapest': EuropeBerlin,
+    'Europe/Busingen': EuropeBerlin,
+    'Europe/Copenhagen': EuropeBerlin,
+    'Europe/Gibraltar': EuropeBerlin,
+    'Europe/Ljubljana': EuropeBerlin,
+    'Europe/Luxembourg': EuropeBerlin,
+    'Europe/Madrid': EuropeBerlin,
+    'Europe/Malta': EuropeBerlin,
+    'Europe/Monaco': EuropeBerlin,
+    'Europe/Oslo': EuropeBerlin,
+    'Europe/Paris': EuropeBerlin,
+    'Europe/Podgorica': EuropeBerlin,
+    'Europe/Prague': EuropeBerlin,
+    'Europe/Rome': EuropeBerlin,
+    'Europe/San_Marino': EuropeBerlin,
+    'Europe/Sarajevo': EuropeBerlin,
+    'Europe/Skopje': EuropeBerlin,
+    'Europe/Stockholm': EuropeBerlin,
+    'Europe/Tirane': EuropeBerlin,
+    'Europe/Vaduz': EuropeBerlin,
+    'Europe/Vatican': EuropeBerlin,
+    'Europe/Vienna': EuropeBerlin,
+    'Europe/Warsaw': EuropeBerlin,
+    'Europe/Zagreb': EuropeBerlin,
+    'Europe/Zurich': EuropeBerlin,
+    'MET': EuropeBerlin,
+    'Poland': EuropeBerlin,
 
     // Nonconforming EET -> EEST
-    'Africa/Cairo': 'Africa/Cairo',
-    'Asia/Gaza': 'Asia/Gaza',
-    'Asia/Hebron': 'Asia/Gaza',
-    'Egypt': 'Africa/Cairo',
+    'Africa/Cairo': AfricaCairo,
+    'Asia/Gaza': AsiaGaza,
+    'Asia/Hebron': AsiaGaza,
+    'Egypt': AfricaCairo,
 
     // EET -> EEST
-    'Asia/Beirut': 'Europe/Athens',
-    'Asia/Famagusta': 'Europe/Athens',
-    'Asia/Nicosia': 'Europe/Athens',
-    'EET': 'Europe/Athens',
-    'Europe/Athens': 'Europe/Athens',
-    'Europe/Bucharest': 'Europe/Athens',
-    'Europe/Chisinau': 'Europe/Athens',
-    'Europe/Helsinki': 'Europe/Athens',
-    'Europe/Kiev': 'Europe/Athens',
-    'Europe/Kyiv': 'Europe/Athens',
-    'Europe/Mariehamn': 'Europe/Athens',
-    'Europe/Nicosia': 'Europe/Athens',
-    'Europe/Riga': 'Europe/Athens',
-    'Europe/Sofia': 'Europe/Athens',
-    'Europe/Tallinn': 'Europe/Athens',
-    'Europe/Tiraspol': 'Europe/Athens',
-    'Europe/Uzhgorod': 'Europe/Athens',
-    'Europe/Vilnius': 'Europe/Athens',
-    'Europe/Zaporozhye': 'Europe/Athens',
+    'Asia/Beirut': EuropeAthens,
+    'Asia/Famagusta': EuropeAthens,
+    'Asia/Nicosia': EuropeAthens,
+    'EET': EuropeAthens,
+    'Europe/Athens': EuropeAthens,
+    'Europe/Bucharest': EuropeAthens,
+    'Europe/Chisinau': EuropeAthens,
+    'Europe/Helsinki': EuropeAthens,
+    'Europe/Kiev': EuropeAthens,
+    'Europe/Kyiv': EuropeAthens,
+    'Europe/Mariehamn': EuropeAthens,
+    'Europe/Nicosia': EuropeAthens,
+    'Europe/Riga': EuropeAthens,
+    'Europe/Sofia': EuropeAthens,
+    'Europe/Tallinn': EuropeAthens,
+    'Europe/Tiraspol': EuropeAthens,
+    'Europe/Uzhgorod': EuropeAthens,
+    'Europe/Vilnius': EuropeAthens,
+    'Europe/Zaporozhye': EuropeAthens,
 
     // Israel
-    'Asia/Jerusalem': 'Asia/Jerusalem',
-    'Asia/Tel_Aviv': 'Asia/Jerusalem',
-    'Israel': 'Asia/Jerusalem',
+    'Asia/Jerusalem': AsiaJerusalem,
+    'Asia/Tel_Aviv': AsiaJerusalem,
+    'Israel': AsiaJerusalem,
 
     // ACST -> ACDT
-    'Australia/Adelaide': 'Australia/Adelaide',
-    'Australia/Broken_Hill': 'Australia/Adelaide',
-    'Australia/South': 'Australia/Adelaide',
-    'Australia/Yancowinna': 'Australia/Adelaide',
+    'Australia/Adelaide': AustraliaAdelaide,
+    'Australia/Broken_Hill': AustraliaAdelaide,
+    'Australia/South': AustraliaAdelaide,
+    'Australia/Yancowinna': AustraliaAdelaide,
 
     // AEST -> AEDT
-    'Antarctica/Macquarie': 'Australia/Sydney',
-    'Australia/ACT': 'Australia/Sydney',
-    'Australia/Canberra': 'Australia/Sydney',
-    'Australia/Currie': 'Australia/Sydney',
-    'Australia/Hobart': 'Australia/Sydney',
-    'Australia/Melbourne': 'Australia/Sydney',
-    'Australia/NSW': 'Australia/Sydney',
-    'Australia/Sydney': 'Australia/Sydney',
-    'Australia/Tasmania': 'Australia/Sydney',
-    'Australia/Victoria': 'Australia/Sydney',
+    'Antarctica/Macquarie': AustraliaSydney,
+    'Australia/ACT': AustraliaSydney,
+    'Australia/Canberra': AustraliaSydney,
+    'Australia/Currie': AustraliaSydney,
+    'Australia/Hobart': AustraliaSydney,
+    'Australia/Melbourne': AustraliaSydney,
+    'Australia/NSW': AustraliaSydney,
+    'Australia/Sydney': AustraliaSydney,
+    'Australia/Tasmania': AustraliaSydney,
+    'Australia/Victoria': AustraliaSydney,
 
     // +10:30 -> +11:30
-    'Australia/LHI': 'Australia/Lord_Howe',
-    'Australia/Lord_Howe': 'Australia/Lord_Howe',
+    'Australia/LHI': AustraliaLordHowe,
+    'Australia/Lord_Howe': AustraliaLordHowe,
 
     // +11 -> +12
-    'Pacific/Norfolk': 'Pacific/Norfolk',
+    'Pacific/Norfolk': PacificNorfolk,
 
     // NZST -> NZDT
-    'Antarctica/McMurdo': 'Pacific/Auckland',
-    'Antarctica/South_Pole': 'Pacific/Auckland',
-    'NZ': 'Pacific/Auckland',
-    'Pacific/Auckland': 'Pacific/Auckland',
+    'Antarctica/McMurdo': PacificAuckland,
+    'Antarctica/South_Pole': PacificAuckland,
+    'NZ': PacificAuckland,
+    'Pacific/Auckland': PacificAuckland
 
-    // +12:45 -> +13:45
-    'NZ-CHAT': 'Pacific/Chatham',
-    'Pacific/Chatham': 'Pacific/Chatham'
   };
 
   const roundTimeZoneRepresentatives = {
