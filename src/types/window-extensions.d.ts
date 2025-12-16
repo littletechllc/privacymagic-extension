d860fb3b54e27d5d504e6dfff937fef035d3b2b6
Arthur Edelstein <arthuredelstein@gmail.com>
2025-12-16 12:22:16 -0800

    handle BatteryManager type

--- src/types/window-extensions.d.ts
+++ src/types/window-extensions.d.ts
@@ -1,11 +1,18 @@
 export {};
 
 declare global {
+  class BatteryManager extends EventTarget {
+    charging: boolean;
+    chargingTime: number;
+    dischargingTime: number;
+    level: number;
+  }
   class DevicePosture extends EventTarget {
     readonly type: string;
   }
 
   interface Window {
+    BatteryManager?: typeof BatteryManager;
     DevicePosture?: typeof DevicePosture;
   }
 }
