5d0a959672b7db31872f84e5ef5dceb3a801225b
Arthur Edelstein <arthur@littletechllc.com>
2025-12-16 18:16:46 -0800

    simplify http-warning code

--- src/background/http-warning.js
+++ src/background/http-warning.js
@@ -3,53 +3,42 @@
 import { addIfMissing, removeIfPresent } from '../common/util.js';
 
 const HTTP_WARNING_MAIN_RULE_ID = 300;
+const HTTP_WARNING_URL = chrome.runtime.getURL('/privacymagic/http-warning.html');
+
+/** @type {chrome.declarativeNetRequest.Rule} */
+const mainRule = {
+  id: HTTP_WARNING_MAIN_RULE_ID,
+  action: {
+    type: 'redirect',
+    redirect: {
+      regexSubstitution: HTTP_WARNING_URL + '?url=\\0'
+    }
+  },
+  priority: 3,
+  condition: {
+    regexFilter: '^http://.*',
+    resourceTypes: ['main_frame']
+  }
+};
+
+const updateMainRule = async () => {
+  await chrome.declarativeNetRequest.updateSessionRules({ removeRuleIds: [HTTP_WARNING_MAIN_RULE_ID], addRules: [mainRule] });
+};
 
 export const updateHttpWarningNetworkRuleException = async (domain, value) => {
   console.log('updating http warning network rule exception for domain:', domain, 'value:', value);
-  const rules = await chrome.declarativeNetRequest.getSessionRules({
-    ruleIds: [HTTP_WARNING_MAIN_RULE_ID]
-  });
-  console.log('rules:', rules);
-  if (rules.length === 0) {
-    return;
-  }
-  const rule = rules[0];
-  const excludedRequestDomains = rule.condition.excludedRequestDomains || [];
-  console.log('excludedRequestDomains:', excludedRequestDomains);
+  const excludedRequestDomains = mainRule.condition.excludedRequestDomains || [];
   if (value === false || value === 'exception') {
     addIfMissing(excludedRequestDomains, domain);
   } else {
     removeIfPresent(excludedRequestDomains, domain);
   }
-  rule.condition.excludedRequestDomains = excludedRequestDomains;
-  console.log('new http warning main rule:', rule);
-  await chrome.declarativeNetRequest.updateSessionRules({ removeRuleIds: [rule.id], addRules: [rule] });
-  const rules2 = await chrome.declarativeNetRequest.getSessionRules({
-    ruleIds: [HTTP_WARNING_MAIN_RULE_ID]
-  });
-  console.log('rules2:', rules2);
+  mainRule.condition.excludedRequestDomains = excludedRequestDomains;
+  await updateMainRule();
 };
 
 export const createHttpWarningNetworkRule = async () => {
-  const httpWarningUrl = chrome.runtime.getURL('/privacymagic/http-warning.html');
-  const mainRule = {
-    id: HTTP_WARNING_MAIN_RULE_ID,
-    action: {
-      type: 'redirect',
-      redirect: {
-        regexSubstitution: httpWarningUrl + '?url=\\0'
-      }
-    },
-    priority: 3,
-    condition: {
-      regexFilter: '^http://.*',
-      resourceTypes: ['main_frame']
-    }
-  };
-  await chrome.declarativeNetRequest.updateSessionRules({
-    removeRuleIds: [HTTP_WARNING_MAIN_RULE_ID],
-    addRules: [mainRule]
-  });
+  await updateMainRule();
 
   if (chrome.declarativeNetRequest.onRuleMatchedDebug) {
     chrome.declarativeNetRequest.onRuleMatchedDebug.addListener(
