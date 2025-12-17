d00638aada57980f4a05013cf47c2261a9e3aba8
Arthur Edelstein <arthur@littletechllc.com>
2025-12-16 16:46:24 -0800

    more experimental types

--- src/types/window-extensions.d.ts
+++ src/types/window-extensions.d.ts
@@ -1,18 +1,64 @@
 export {};
 
 declare global {
+
   class BatteryManager extends EventTarget {
     charging: boolean;
     chargingTime: number;
     dischargingTime: number;
     level: number;
   }
+
   class DevicePosture extends EventTarget {
     readonly type: string;
+  }
+
+  class LayoutShift extends PerformanceEntry {
+    readonly lastInputTime: number;
+  }
+
+  class NavigatorUAData extends EventTarget {
+    readonly brands: { brand: string; version: string }[];
+    readonly mobile: boolean;
+    readonly platform: string;
+    getHighEntropyValues: () => Promise<{ architecture: string; bitness: string; brands: { brand: string; version: string }[]; formFactors: string[]; fullVersionList: { brand: string; version: string }[]; mobile: boolean; model: string; platform: string; platformVersion: string; uaFullVersion: string; wow64: boolean }>;
+    toJSON: () => {};
+  }
+
+  class PerformanceLongAnimationFrameTiming extends PerformanceEntry {
+    blockingDuration: number;
+    firstUIEventTimestamp: number;
+    renderStart: number;
+    styleAndLayoutStart: number;
+  }
+
+  class PerformanceLongTaskTiming extends PerformanceEntry {
+    duration: number;
+    startTime: number;
+  }
+
+  class PerformanceScriptTiming extends PerformanceEntry {
+    executionStart: number;
+    forcedStyleAndLayoutDuration: number;
+    pauseDuration: number;
+  }
+
+  class SharedStorage extends EventTarget {
+    has: (key: string) => boolean;
+    get: (key: string) => string | null;
+    set: (key: string, value: string) => void;
+    delete: (key: string) => void;
+    clear: () => void;
   }
 
   interface Window {
     BatteryManager?: typeof BatteryManager;
     DevicePosture?: typeof DevicePosture;
+    LayoutShift?: typeof LayoutShift;
+    PerformanceLongAnimationFrameTiming?: typeof PerformanceLongAnimationFrameTiming;
+    PerformanceLongTaskTiming?: typeof PerformanceLongTaskTiming;
+    PerformanceScriptTiming?: typeof PerformanceScriptTiming;
+    NavigatorUAData?: typeof NavigatorUAData;
+    SharedStorage?: typeof SharedStorage;
   }
 }
