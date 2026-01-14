9813e827787c62ff893725aff594dcad7b93487e
Arthur Edelstein <arthur@littletechllc.com>
2026-01-14 12:06:28 -0800

    no multiple empty lines

--- eslint.config.js
+++ eslint.config.js
@@ -14,11 +14,12 @@ export default [
       }
     },
     rules: {
-      eqeqeq: ["error", "always", { null: "ignore" }],
-      "no-implicit-coercion": "error",
+      "eqeqeq": ["error", "always", { null: "ignore" }],
       "no-cond-assign": ["error", "always"],
       "no-constant-condition": "error",
       "no-fallthrough": "error",
+      "no-implicit-coercion": "error",
+      "no-multiple-empty-lines": "error",
       "no-prototype-builtins": "error",
       "no-trailing-spaces": "error",
       "use-isnan": "error",
