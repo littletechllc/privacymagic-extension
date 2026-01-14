a63e138f5c677a9226e6f23f5ee78dadac2ee3dd
Arthur Edelstein <arthur@littletechllc.com>
2026-01-13 22:43:17 -0800

    secret.ts for shared secret

---
+++ src/content_scripts/secret.ts
@@ -0,0 +1,15 @@
+let value : string | undefined = undefined
+export const sharedSecret = () => {
+  if (value !== undefined) {
+    return value
+  }
+  if (document.documentElement.dataset.sharedSecret === undefined) {
+    value = crypto.randomUUID()
+    document.documentElement.dataset.sharedSecret = value
+    return value
+  } else {
+    value = document.documentElement.dataset.sharedSecret
+    document.documentElement.dataset.sharedSecret = undefined
+    return value
+  }
+}
