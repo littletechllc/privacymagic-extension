d175da4b78a3bf7695182bdd3d6458d9b23e7c20
Arthur Edelstein <arthuredelstein@gmail.com>
2026-01-14 10:38:03 -0800

    fix id usage

--- src/content_scripts/background-fetch.ts
+++ src/content_scripts/background-fetch.ts
@@ -11,13 +11,11 @@ type FetchID = string
 // Send a request to the isolated world to fetch the content of the URL.
 // The request is sent using a custom event whose name is `fetch_${secret}`,
 // with an associated id.
-const sendBackgroundFetchRequest = (url: string): FetchID => {
-  const id: FetchID = crypto.randomUUID()
+const sendBackgroundFetchRequest = (url: string, id: FetchID): void => {
   dispatchEventSafe(
     documentSafe,
     new CustomEventSafe(`fetch_${secret}`,
       { detail: { url, id } }))
-      return id
 }
 
 // This function is used to send the response back to the main world using a custom event
@@ -44,7 +42,8 @@ export const handleBackgroundFetchRequests = (fetchFunction: (url: string) => Pr
 // This function returns a promise that resolves to the content of the fetched URL.
 // Under the hood, it sends a request to the isolated world and waits for the response
 // using a custom event whose name is `response_${secret}_${id}`.
-export const backgroundFetch : (url: string) => Promise<string> = async (url: string) => {
+export const backgroundFetch: (url: string) => Promise<string> = async (url: string) => {
+  const id: FetchID = crypto.randomUUID()
   const contentPromise = new Promise<string>((resolve) => {
     document.addEventListener(`response_${secret}_${id}`, (event) => {
       if (event instanceof CustomEvent) {
@@ -53,6 +52,6 @@ export const backgroundFetch : (url: string) => Promise<string> = async (url: st
       }
     }, { once: true })
   })
-  const id: FetchID = sendBackgroundFetchRequest(url)
+  sendBackgroundFetchRequest(url, id)
   return contentPromise
 }
