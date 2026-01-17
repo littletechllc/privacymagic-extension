67be0f8c4f9a7c5d235655a33603c29c537a26fd
Arthur Edelstein <arthuredelstein@gmail.com>
2026-01-17 13:22:31 -0800

    handle when self.document etc is undefined

--- src/content_scripts/background-fetch-main.ts
+++ src/content_scripts/background-fetch-main.ts
@@ -7,6 +7,9 @@ const secret = sharedSecret()
 // The request is sent using a custom event whose name is `fetch_${secret}`,
 // with an associated id.
 const sendBackgroundFetchRequest = (url: string, id: FetchID): void => {
+  if (dispatchEventSafe === undefined || documentSafe === undefined || CustomEventSafe === undefined) {
+    throw new Error('document not available')
+  }
   dispatchEventSafe(
     documentSafe,
     new CustomEventSafe(`fetch_${secret}`,
@@ -17,6 +20,9 @@ const sendBackgroundFetchRequest = (url: string, id: FetchID): void => {
 // event whose name is `response_${secret}_${id}`. Returns a promise that
 // resolves to the content of the fetched URL.
 export const backgroundFetch: (url: string) => Promise<string> = async (url: string) => {
+  if (secret === undefined) {
+    throw new Error('Shared secret is not available')
+  }
   const id: FetchID = crypto.randomUUID()
   const contentPromise = new Promise<string>((resolve) => {
     document.addEventListener(`response_${secret}_${id}`, (event) => {
