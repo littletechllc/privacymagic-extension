7573105e0ca15133510aa69341d1defacab7d827
Arthur Edelstein <arthuredelstein@gmail.com>
2026-01-20 13:59:56 -0800

    consolidate master and ads to allow-rules.ts

---
+++ src/background/allow-rules.ts
@@ -0,0 +1,31 @@
+import { ALL_RESOURCE_TYPES, updateListOfExceptions } from "../common/util";
+import { DNR_RULE_PRIORITIES, dnrRuleIdForName } from "./dnr-rule-parameters";
+import { SettingsId } from "../common/settings-ids";
+
+const cachedMasterSwitchRules: Partial<Record<SettingsId, chrome.declarativeNetRequest.Rule>> = {
+  masterSwitch: {
+    id: dnrRuleIdForName('allow_rule|masterSwitch'),
+    priority: DNR_RULE_PRIORITIES.MASTER_SWITCH,
+    action: { type: 'allow' },
+    condition: { topDomains: undefined, resourceTypes: ALL_RESOURCE_TYPES }
+  },
+  ads: {
+    id: dnrRuleIdForName('allow_rule|ads'),
+    priority: DNR_RULE_PRIORITIES.BLOCKER_EXCEPTIONS,
+    action: { type: 'allow' },
+    condition: { topDomains: undefined, resourceTypes: ALL_RESOURCE_TYPES }
+  }
+}
+
+export const updateMasterSwitchRule = async (domain: string,setting: SettingsId, value: boolean): Promise<void> => {
+  const rule = cachedMasterSwitchRules[setting]
+  if (rule === undefined) {
+    return
+  }
+  rule.condition.topDomains = updateListOfExceptions<string>(rule.condition.topDomains, domain, value)
+  if (rule.condition.topDomains === undefined || rule.condition.topDomains.length === 0) {
+    await chrome.declarativeNetRequest.updateSessionRules({ removeRuleIds: [rule.id], addRules: [] })
+  } else {
+    await chrome.declarativeNetRequest.updateSessionRules({ removeRuleIds: [rule.id], addRules: [rule] })
+  }
+}
