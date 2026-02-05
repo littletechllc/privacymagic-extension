74a9a9f4a206cff9ebbe57fb038fc22ba1ecf26e
Arthur Edelstein <arthur@littletechllc.com>
2026-02-04 18:11:56 -0800

    lint fixes

--- src/content_scripts/patches/useragent.ts
+++ src/content_scripts/patches/useragent.ts
@@ -3,9 +3,10 @@ import { redefinePropertyValues } from '@src/content_scripts/helpers/monkey-patc
 const useragent = (): void => {
   const originalBrands = navigator.userAgentData?.brands ?? []
   const chromiumVersion = originalBrands.find(brand => brand.brand === 'Chromium')?.version ?? '0.0.0.0'
-  const platformVersion = navigator.userAgentData?.platformVersion ?? '0.0.0.0'
-  const fakeChromiumVersion = `${chromiumVersion.split('.')[0]}.0.0.0`
-  const fakeNotABrandVersion = '8.0.0.0'
+  const uaData = navigator.userAgentData as { platformVersion?: string } | undefined
+  const _platformVersion: string = uaData?.platformVersion ?? '0.0.0.0'
+  const _fakeChromiumVersion = `${chromiumVersion.split('.')[0]}.0.0.0`
+  const _fakeNotABrandVersion = '8.0.0.0'
   const navigatorPrototype = self.Navigator ?? self.WorkerNavigator
   if (navigatorPrototype == null) {
     return
