9d9f924087e6cbb66bafb2dcdb0aecf109516f1f
Arthur Edelstein <arthur@littletechllc.com>
2026-02-08 18:34:28 -0800

    build.sh

---
+++ math/build.sh
@@ -0,0 +1,11 @@
+emcc stub.c \
+  -Oz \
+  -s STANDALONE_WASM \
+  -fno-builtin \
+  -fno-exceptions \
+  -Wl,--no-entry \
+  -Wl,--strip-all \
+  -Wl,--gc-sections \
+  -Wl,--lto-O3 \
+  -s EXPORTED_FUNCTIONS='["_acos","_acosh","_asin","_asinh","_atan","_atanh","_cbrt","_cos","_cosh","_exp","_expm1","_log","_log1p","_log2","_log10","_sin","_sinh","_sqrt","_tan","_tanh","_atan2","_hypot","_pow"]' \
+  -o math.wasm
