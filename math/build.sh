1f9c7821844f92939dbe9866db2fc486348b3df9
Arthur Edelstein <arthuredelstein@gmail.com>
2026-02-09 13:02:24 -0800

    wasm Math.* implementation

--- math/build.sh
+++ math/build.sh
@@ -1,4 +1,4 @@
-emcc stub.c \
+emcc math.c \
   -Oz \
   -s STANDALONE_WASM \
   -fno-builtin \
@@ -7,5 +7,6 @@ emcc stub.c \
   -Wl,--strip-all \
   -Wl,--gc-sections \
   -Wl,--lto-O3 \
-  -s EXPORTED_FUNCTIONS='["_acos","_acosh","_asin","_asinh","_atan","_atanh","_cbrt","_cos","_cosh","_exp","_expm1","_log","_log1p","_log2","_log10","_sin","_sinh","_sqrt","_tan","_tanh","_atan2","_hypot","_pow"]' \
+  -s MALLOC='emmalloc' \
+  -s EXPORTED_FUNCTIONS='["_malloc","_free","_acos","_acosh","_asin","_asinh","_atan","_atanh","_cbrt","_cos","_cosh","_exp","_expm1","_log","_log1p","_log2","_log10","_sin","_sinh","_sqrt","_tan","_tanh","_atan2","_math_hypot","_pow"]' \
   -o math.wasm
