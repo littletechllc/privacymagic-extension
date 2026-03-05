762024dee76154b9a4ed37633ed861dd836ef2f9
Arthur Edelstein <arthuredelstein@gmail.com>
2026-03-04 16:48:15 -0800

    fix broken worker patch

--- src/content_scripts/patches/display.ts
+++ src/content_scripts/patches/display.ts
@@ -2,6 +2,9 @@ import { spoofMediaQuery } from '@src/content_scripts/helpers/helpers'
 import { GlobalScope } from '../helpers/globalObject'
 
 const display = (globalObject: GlobalScope): void => {
+  if (globalObject.matchMedia === undefined) {
+    return
+  }
   spoofMediaQuery(globalObject, 'prefers-reduced-motion', 'no-preference')
 }
 
