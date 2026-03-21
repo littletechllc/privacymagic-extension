f457d889d4e8b1701278e761e7c606e275b1f28b
Arthur Edelstein <arthur@littletechllc.com>
2026-03-21 00:24:55 -0700

    welcome steps close and reopen

--- src/privacymagic/welcome.ts
+++ src/privacymagic/welcome.ts
@@ -1,24 +1,49 @@
 console.log('welcome.ts loaded');
 
-const step1 = document.getElementById('step1')
+const step: (HTMLElement | null)[] = [null]
+step[1] = document.getElementById('step1')
+step[2] = document.getElementById('step2')
+step[3] = document.getElementById('step3')
 
-const updateStep1 = (isOnToolbar: boolean) => {
-  if (isOnToolbar) {
-    step1?.classList.add('step-card-completed')
+const updateStep = (id: number, completed: boolean) => {
+  if (completed) {
+    step[id]?.classList.add('step-card-completed')
   } else {
-    step1?.classList.remove('step-card-completed')
+    step[id]?.classList.remove('step-card-completed')
   }
 }
 
 chrome.action.onUserSettingsChanged.addListener(
   (details) => {
     console.log('User settings changed:', details);
-    updateStep1(details.isOnToolbar ?? false)
+    updateStep(1, details.isOnToolbar ?? false)
   }
 )
 
 chrome.action.getUserSettings().then((userSettings) => {
-  updateStep1(userSettings.isOnToolbar ?? false)
+  updateStep(1, userSettings.isOnToolbar ?? false)
 }).catch((error) => {
   console.error('Error getting user settings:', error)
+})
+
+document.querySelector('#step2 .btn-secondary')
+  ?.addEventListener('click', (event: Event) => {
+    event.preventDefault()
+    event.stopPropagation()
+  updateStep(2, true)
+})
+
+document.querySelector('#step3 .btn-secondary')
+ ?.addEventListener('click', (event: Event) => {
+  event.preventDefault()
+  event.stopPropagation()
+  updateStep(3, true)
+})
+
+document.querySelectorAll('.step-header').forEach((stepHeader, index) => {
+  stepHeader.addEventListener('click', (event: Event) => {
+    event.preventDefault()
+    event.stopPropagation()
+    updateStep(index + 1, false)
+  })
 })
