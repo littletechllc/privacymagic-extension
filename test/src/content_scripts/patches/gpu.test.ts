13d0ac906658db6946f0a88a9e88ed3eb2e3b227
Arthur Edelstein <arthur@littletechllc.com>
2026-03-25 11:46:09 -0700

    remove unused import

--- test/src/content_scripts/patches/gpu.test.ts
+++ test/src/content_scripts/patches/gpu.test.ts
@@ -1,4 +1,4 @@
-import {describe, it, expect, beforeEach } from '@jest/globals'
+import {describe, it, expect } from '@jest/globals'
 import gpu from '@src/content_scripts/patches/gpu'
 
 describe('gpu patch', () => {
