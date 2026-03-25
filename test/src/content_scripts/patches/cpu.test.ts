841cb5e2a151b8dc2b0e25a6ef8cc910fa5e5c4d
Arthur Edelstein <arthur@littletechllc.com>
2026-03-24 17:07:23 -0700

    remove unused test

--- test/src/content_scripts/patches/cpu.test.ts
+++ test/src/content_scripts/patches/cpu.test.ts
@@ -3,20 +3,17 @@ import cpu from '@src/content_scripts/patches/cpu'
 import { defineMockProperties } from '@test/mocks/define'
 
 const mockHardwareConcurrency = 8
-const mockCpuClass = 'arm'
 
 describe('cpu patch', () => {
   beforeEach(() => {
     defineMockProperties(self.Navigator.prototype, {
       hardwareConcurrency: mockHardwareConcurrency,
-      cpuClass: mockCpuClass
     })
   })
 
   describe('without patch', () => {
     it('should return original hardwareConcurrency and cpuClass', () => {
       expect(navigator.hardwareConcurrency).toBe(mockHardwareConcurrency)
-      expect((navigator as { cpuClass?: string }).cpuClass).toBe(mockCpuClass)
     })
   })
 
@@ -27,10 +24,6 @@ describe('cpu patch', () => {
 
     it('should set hardwareConcurrency to 4', () => {
       expect(navigator.hardwareConcurrency).toBe(4)
-    })
-
-    it('should set cpuClass to undefined', () => {
-      expect((navigator as { cpuClass?: string }).cpuClass).toBeUndefined()
     })
   })
 })
