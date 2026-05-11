7cd5e84e597689ad4ea9e4a967ac03ca54f55178
Arthur Edelstein <arthur@littletechllc.com>
2026-05-11 14:43:28 -0700

    filter-list-paths.ts

---
+++ src/common/filter-list-paths.ts
@@ -0,0 +1,6 @@
+export const FILTER_LIST_DIR = 'filter_list'
+export const NETWORK_RULES_FILE = 'network_rules.json'
+export const NETWORK_RULES_PATH = `${FILTER_LIST_DIR}/${NETWORK_RULES_FILE}`
+export const COSMETIC_FILTERS_DIR = `${FILTER_LIST_DIR}/cosmetic_filters`
+export const SCRIPTLETS_DIR = `${FILTER_LIST_DIR}/scriptlets`
+export const PROCEDURAL_FILTERS_DIR = `${FILTER_LIST_DIR}/procedural_filters`
