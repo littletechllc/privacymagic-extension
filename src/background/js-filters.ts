2ac2cd0667d641bdf2d022fff506a5d3c85c6f62
Arthur Edelstein <arthur@littletechllc.com>
2026-05-11 18:06:58 -0700

    merge readIndexFile back to js-filters.ts

--- src/background/js-filters.ts
+++ src/background/js-filters.ts
@@ -3,7 +3,27 @@ import { logError } from '@src/common/util'
 import { PROCEDURAL_FILTERS_DIR, SCRIPTLETS_DIR } from '@src/common/filter-list-paths'
 import { type DisabledSettingCollection } from '@src/common/settings-read'
 import { unique } from '@src/common/data-structures'
-import { readIndexFile } from './filter-util'
+
+const fetchLocalFile = async (path: string): Promise<string> => {
+  const url = chrome.runtime.getURL(path)
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw new Error(`Failed to fetch local file: ${path}: ${response.statusText}`)
+  }
+  return await response.text()
+}
+
+const indexFileCache = new Map<string, Promise<Set<string>>>()
+
+const readIndexFile = async (dir: string): Promise<Set<string>> => {
+  if (!indexFileCache.has(dir)) {
+    indexFileCache.set(dir, fetchLocalFile(`${dir}/index.txt`).then(indexFile => {
+      const domains = indexFile.split('\n')
+      return new Set(domains.map(domain => domain.trim()).filter(domain => domain !== ''))
+    }))
+  }
+  return await indexFileCache.get(dir)!
+}
 
 const getId = (dir: string, domain: string): string => {
   return `${dir}/${domain}`
