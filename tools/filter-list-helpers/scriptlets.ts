b46f850897d8d7347bbd6bcf2b95ccdfcebd222d
Arthur Edelstein <arthur@littletechllc.com>
2026-05-22 23:37:52 -0700

    handle set-cookie and remove-cookie as special cases

--- tools/filter-list-helpers/scriptlets.ts
+++ tools/filter-list-helpers/scriptlets.ts
@@ -73,27 +73,63 @@ const collectScriptletsForDomains = (lines: string[]): Record<string, ScriptletC
   return scriptletsForDomains
 }
 
+const createCookieRule = (domain: string, headerValue: string, id: number): chrome.declarativeNetRequest.Rule => {
+  return {
+    id,
+    action: {
+      type: 'modifyHeaders',
+      responseHeaders: [{
+        operation: 'append',
+        header: 'Set-Cookie',
+        value: headerValue
+      }]
+    },
+    priority: DNR_RULE_PRIORITIES.STATIC_RULES,
+    condition: {
+      urlFilter: `||${domain}`,
+      resourceTypes: ['main_frame', 'sub_frame']
+    }
+  }
+}
+
+const appendCookieRule = (domain: string, cookieKey: string, cookieValue: string, id: number): chrome.declarativeNetRequest.Rule => {
+  const headerValue = `${cookieKey}=${cookieValue}; Secure; SameSite=None; Path=/; Partitioned`
+  return createCookieRule(domain, headerValue, id)
+}
+
+const removeCookieRule = (domain: string, cookieKey: string, id: number): chrome.declarativeNetRequest.Rule => {
+  const headerValue = `${cookieKey}=; Max-Age=0; Secure; SameSite=None; Path=/; Partitioned`
+  return createCookieRule(domain, headerValue, id)
+}
+
+const isCookieScriptlet = (scriptlet: ScriptletCommand): boolean => {
+  return scriptlet[0] === 'set-cookie' || scriptlet[0] === 'remove-cookie'
+}
+
 const generateScriptletRules = (scriptletsForDomains: Record<string, ScriptletCommand[]>): chrome.declarativeNetRequest.Rule[] => {
   const rules: chrome.declarativeNetRequest.Rule[] = []
-  let id = 0
+  let id = 1
   for (const [domain, scriptlets] of entries(scriptletsForDomains)) {
-    ++id
-    rules.push({
-      id,
-      action: {
-        type: 'modifyHeaders',
-        responseHeaders: [{
-          operation: 'append',
-          header: 'Set-Cookie',
-          value: `${SCRIPTLET_COOKIE_KEY}=${toBase64(JSON.stringify(scriptlets))}; Secure; SameSite=None; Path=/; Partitioned`
-        }]
-      },
-      priority: DNR_RULE_PRIORITIES.STATIC_RULES,
-      condition: {
-        urlFilter: `||${domain}`,
-        resourceTypes: ['main_frame', 'sub_frame']
+    const noncookieScriptlets = scriptlets.filter(scriptlet => !isCookieScriptlet(scriptlet))
+    if (noncookieScriptlets.length > 0) {
+      const cookieValue = toBase64(JSON.stringify(noncookieScriptlets))
+      rules.push(appendCookieRule(domain, SCRIPTLET_COOKIE_KEY, cookieValue, id))
+      ++id
+    }
+    const cookieScriptlets = scriptlets.filter(isCookieScriptlet)
+    if (cookieScriptlets.length > 0) {
+      for (const cookieScriptlet of cookieScriptlets) {
+        const [scriptletName, ...args] = cookieScriptlet
+        if (scriptletName === 'set-cookie') {
+          const [cookieKey, cookieValue] = args
+          rules.push(appendCookieRule(domain, cookieKey, cookieValue, id))
+        } else if (scriptletName === 'remove-cookie') {
+          const [cookieKey] = args
+          rules.push(removeCookieRule(domain, cookieKey, id))
+        }
+        ++id
       }
-    })
+    }
   }
   return rules
 }
